8ea7576ad2435c7dd43724431e415591894ccea7
usimebul <usimebul@signgate.com>
2026-04-07 11:08:41 +0900

    feat: add push notification service using web-push

--- apps/charger/services/pushService.ts
+++ apps/charger/services/pushService.ts
@@ -1,13 +1,14 @@
 import { PushMessage } from '@/app/(home)/_components/types';
 import webpush from 'web-push';
 
-// webpush.setVapidDetails(
-//     process.env.VAPID_SUBJECT as string,
-//     process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY as string,
-//     process.env.VAPID_PRIVATE_KEY as string
-// );
 
 export async function push(subscription: PushSubscriptionJSON, message: PushMessage) {
+    webpush.setVapidDetails(
+        process.env.VAPID_SUBJECT as string,
+        process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY as string,
+        process.env.VAPID_PRIVATE_KEY as string
+    );
+
     try {
         await webpush.sendNotification(subscription as webpush.PushSubscription, JSON.stringify(message));
         console.log("푸시 전송 성공!");
