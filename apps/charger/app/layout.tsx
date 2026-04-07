525891340dd4caf4d426bbdb60ae90ba5f3c71a3
usimebul <usimebul@signgate.com>
2026-04-07 10:58:57 +0900

    feat: add root layout with analytics and implement web-push notification service

--- apps/charger/app/layout.tsx
+++ apps/charger/app/layout.tsx
@@ -1,10 +1,10 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/next"
-
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Providers from "./providers";
+import type { Viewport } from 'next'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,14 +23,17 @@ export const metadata: Metadata = {
   title: "다우디지털스퀘어 충전 현황 서비스",
   description: "다우디지털스퀘어 충전 현황 및 충전 대기 알림 서비스",
   manifest: "/manifest.json"
-
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: 'cover',
+}
+
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} ${daki.variable}`}>
--- apps/charger/services/pushService.ts
+++ apps/charger/services/pushService.ts
@@ -1,11 +1,11 @@
 import { PushMessage } from '@/app/(home)/_components/types';
 import webpush from 'web-push';
 
-webpush.setVapidDetails(
-    process.env.VAPID_SUBJECT as string,
-    process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY as string,
-    process.env.VAPID_PRIVATE_KEY as string
-);
+// webpush.setVapidDetails(
+//     process.env.VAPID_SUBJECT as string,
+//     process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY as string,
+//     process.env.VAPID_PRIVATE_KEY as string
+// );
 
 export async function push(subscription: PushSubscriptionJSON, message: PushMessage) {
     try {
